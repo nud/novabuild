ca1b8605f55fa07242da93c0839722d45374aa54
Support <support@castor.multitel.be>
2008-05-08 17:44:35 +0200

    New method chroot.abspath

--- novabuild/chroot.py
+++ novabuild/chroot.py
@@ -11,6 +11,20 @@ class Chroot(object):
 
         self.root_dir = '/chroots/%s-%s' % (self.user, self.name)
 
+    def abspath(self, path, cwd=None, root=False, internal=True):
+        homedir = root and '/root' or '/home/%s' % self.user
+
+        if path.startswith('~/'):
+            path = os.path.join(homedir, path[2:])
+        elif not path.startswith('/'):
+            if cwd is None: cwd = homedir
+            path = os.path.join(cwd, path)
+
+        if internal:
+            path = os.path.join(self.root_dir, path)
+
+        return path
+
     def get_home_dir(self, root=False):
         if root:
             homedir = 'root'
