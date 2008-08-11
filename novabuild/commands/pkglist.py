ff08282bc6d66b52ad373ab86cbf48ad638a521b
Steve Frécinaux <frecinaux@novacom.be>
2008-08-11 18:17:51 +0200

    Show an header line in 'list' command

--- novabuild/commands/pkglist.py
+++ novabuild/commands/pkglist.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*- ex:set ts=4 et:
 
-from novabuild.config import ModuleSetParser
-
 def list_packages(moduleset):
+    print "Category           Package Name                 Version         Source URI"
+    print "---------          -------------                --------        -----------"
+
     for section in sorted(moduleset.sections()):
         items = dict(moduleset.items(section))
         items['name'] = section
         print "%(category)-18s %(name)-28s %(version)-15s %(source)s" % items
+
 
 def main(moduleset, args):
     list_packages(moduleset)
