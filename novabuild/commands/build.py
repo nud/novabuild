4ce8db0138eccd937fad20a380619712e1bc161b
Steve Frécinaux <frecinaux@novacom.be>
2008-08-12 17:04:23 +0200

    Add basic dependency handling to the 'build' command.
    It still doesn't handle alternative dependencies and versionned
    dependencies.

--- novabuild/commands/build.py
+++ novabuild/commands/build.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*- ex:set ts=4 et:
 
-import os, sys
+import os, sys, glob
 from novabuild.config import ModuleSetParser
+from novabuild.debcontrol import PackageControlParser
 from novabuild.run import system
 from novabuild.colours import red, blue
 from novabuild.chroot import Chroot
@@ -33,17 +34,23 @@ def check_code(code, package):
         raise Exception("Could not build package %s" % package)
 
 
+def get_dependencies(chroot, BUILD_DIR):
+    control = PackageControlParser()
+    control.read(os.path.join(BUILD_DIR, 'debian/control'))
+    dependencies = control.source.get('Build-Depends', '')
+    return [i.strip().split(None, 1)[0] for i in dependencies.split(',')]
+
 def build(chroot, moduleset, package):
     TARBALL = os.path.join('tarballs', moduleset.get(package, 'basename'))
 
     if not os.path.exists(TARBALL):
         raise Exception("You must fetch the package '%s' before building it." % package)
 
-    print blue("Building '%s' '%s'" % (package, moduleset.get(package, 'version')))
-
     BUILD_ROOT = os.path.join(chroot.get_home_dir(), 'tmp-build-dir')
     BUILD_DIR = os.path.join (BUILD_ROOT, '%s-%s' % (package, moduleset.get(package, 'version')))
     DEBIAN_DIR = os.path.join('debian', 'debian-%s' % package)
+
+    print blue("Uncompressing '%s'" % TARBALL)
 
     code = chroot.system('rm -rf /home/%s/tmp-build-dir' % chroot.user, root=True)
     check_code(code, package)
@@ -63,13 +70,15 @@ def build(chroot, moduleset, package):
     else:
         raise Exception("Cannot find the type of the archive.")
 
+
     # Kernel packages have to be handled specially.
     if package == 'linux':
-        code = system('cp %s/config-%s-i386 %s/.config' % (DEBIAN_DIR, moduleset.get(package, 'version'), BUILD_DIR))
+        print blue("Building '%s' '%s'" % (package, moduleset.get(package, 'version')))
+        code = system('cp debian/config-%s-i386 %s/.config' % (moduleset.get(package, 'version'), BUILD_DIR))
         check_code(code, package)
 
         code = chroot.system('make-kpkg --revision=novacom.i386.3.0 kernel_image kernel_headers kernel_source',
-                             cwd='/root/tmp-build-dir', root=True)
+                             cwd=chroot.abspath('~/tmp-build-dir/linux-%s' % moduleset.get(package, 'version')), root=True)
         check_code(code, package)
 
     else:
@@ -79,11 +88,16 @@ def build(chroot, moduleset, package):
         code = system('cp -r %s %s/debian' % (DEBIAN_DIR, BUILD_DIR))
         check_code(code, package)
 
+        print blue('Installing dependencies')
+        code = chroot.system('apt-get install %s' % ' '.join(get_dependencies(chroot, BUILD_DIR)), root=True)
+        check_code(code, package)
+
         # Write a new changelog entry...
         version = "%s-novacom.%s" % (moduleset.get(package, 'version'), moduleset.get(package, 'buildnumber'))
         changelog_file = "%s/debian/changelog" % BUILD_DIR
 
         if not changelog_is_up_to_date(changelog_file, version):
+            print blue("Update ChangeLog")
             old_file = "%s/changelog" % DEBIAN_DIR
             prepend_changelog_entry(changelog_file, old_file, package, version, "* New build.")
 
@@ -91,9 +105,19 @@ def build(chroot, moduleset, package):
             code = system('cp -f %s %s' % (changelog_file, old_file))
             check_code (code, package)
 
+        print blue("Building '%s' '%s'" % (package, moduleset.get(package, 'version')))
         code = chroot.system('dpkg-buildpackage', root=True, cwd='/home/%s/tmp-build-dir/%s-%s'
                              % (chroot.user, package, moduleset.get(package, 'version')))
         check_code(code, package)
+
+        packages_to_install = [i.strip() for i in moduleset.get(package, 'install').split(',')]
+        if packages_to_install != []:
+            print blue("Installing packages '%s'" % "', '".join(packages_to_install))
+            packages = [os.path.basename(i) for i in glob.glob(chroot.abspath('~/tmp-build-dir/*.deb', internal=False))]
+            for package in packages:
+                if package.split('_',1)[0] in packages_to_install:
+                    code = chroot.system("dpkg -i ~/tmp-build-dir/%s" % package)
+                    check_code(code, package)
 
     code = system('mv -f %s/*.deb repository/' % BUILD_ROOT)
 
---
+++ novabuild/debcontrol.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*- ex:set ts=4 sw=4 et:
+
+import sys
+from exceptions import Exception
+
+class ControlSection(dict):
+    """
+    A section of a Debian Control file.
+    """
+    def __init__(self):
+        dict.__init__(self)
+        self._key_order = []
+
+    def __setitem__(self, key, value):
+        dict.__setitem__(self, key, value)
+        if key not in self._key_order:
+            self._key_order.append(key)
+
+    def dump(self):
+        return ''.join("%s: %s\n" % (key, self[key].replace('\n', '\n  ')) for key in self._key_order) + "\n"
+
+class BasicControlParser(object):
+    def __init__(self, defaults=None):
+        self._sections = []
+        self._defaults = {}
+        if defaults:
+            for key, value in defaults.items():
+                self._defaults[self.optionxform(key)] = value
+
+    def optionxform(self, option):
+        return '-'.join(x.capitalize() for x in option.split('-'))
+
+    def append_section(self, section):
+        self._sections.append(section)
+
+    def read(self, filename):
+        fp = file(filename, 'r')
+        self.readfp(fp)
+        fp.close()
+
+    def readfp(self, fp):
+        """Read and parse a debian control file, given a file object."""
+
+        section = None
+        lastkey = None
+        lineno = 0
+
+        for line in fp:
+            line = line.rstrip()
+            lineno += 1
+            if line == '':
+                if section:
+                    self.append_section(section)
+                    section = None
+                    lastkey = None
+                continue
+            
+            if line[0] in ' \t':
+                assert lastkey is not None
+                line = line.lstrip()
+                if line == '.':
+                    section[lastkey] += '\n'
+                else:
+                    section[lastkey] += '\n' + line
+                continue
+
+            assert ':' in line
+            
+            key, value = [bit.strip() for bit in line.split(':', 1)]
+
+            if not section:
+                section = ControlSection()
+
+            section[key] = value
+            lastkey = key
+
+        if section:
+            self.append_section(section)
+
+    def __getitem__(self, item):
+        return self._sections[item]
+
+    def __getslice__(self, a, b):
+        return self._sections[a:b]
+
+    def dump(self):
+        return '\n'.join(section.dump() for section in self._sections)
+
+class PackageControlParser(BasicControlParser):
+    def __init__(self, defaults=None):
+        BasicControlParser.__init__(self, defaults)
+        self.source = None
+
+    def append_section(self, section):
+        if self.source is None:
+            self.source = section
+        else:
+            BasicControlParser.append_section(self, section)
+
+    def dump(self):
+        return self.source.dump() + "\n" + \
+               BasicControlParser.dump(self)
+
+if __name__ == '__main__':
+    c = PackageControlParser()
+    c.read(sys.argv[1])
+    print c.dump()
