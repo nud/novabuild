05645435e8d91a73150d1d41fb94ca1b86d66ee3
Steve Frécinaux <frecinaux@novacom.be>
2008-08-26 14:35:15 +0200

    Kill BUILD_ROOT in 'build'
    It was confusing with BUILD_DIR. Only having one of those makes the code
    cleaner and easier to understand (IMHO)

--- novabuild/commands/build/__init__.py
+++ novabuild/commands/build/__init__.py
@@ -15,15 +15,21 @@ def uncompress_tarball(module, destination):
     if not os.path.exists(filename):
         raise Exception("You must fetch the package '%s' before building it." % module.name)
 
+    dest_parent = os.path.dirname(destination)
+
     print blue("Uncompressing '%s'" % filename)
     if filename.endswith('.tar.bz2') or filename.endswith('.tbz'):
-        check_code(system('tar xjf %s -C %s' % (filename, destination)), module)
+        check_code(system('tar xjf %s -C %s' % (filename, dest_parent)), module)
     elif filename.endswith('.tar.gz') or filename.endswith('.tgz'):
-        check_code(system('tar xzf %s -C %s' % (filename, destination)), module)
+        check_code(system('tar xzf %s -C %s' % (filename, dest_parent)), module)
     elif filename.endswith('.zip'):
-        check_code(system('unzip %s -d %s' % (filename, destination)), module)
+        check_code(system('unzip %s -d %s' % (filename, dest_parent)), module)
     else:
         raise Exception("Cannot find the type of the archive.")
+
+    # Put the directory in the tarball in the right directory
+    code = system('mv %s/* %s' % (dest_parent, destination))
+    #check_code(code, module)  FIXME: we don't want to fail when "cannot move to itself"
 
 
 def get_dependencies(chroot, BUILD_DIR):
@@ -34,17 +40,17 @@ def get_dependencies(chroot, BUILD_DIR):
 
 
 def build(chroot, module):
-    BUILD_ROOT = os.path.join(chroot.get_home_dir(), 'tmp-build-dir')
-    BUILD_DIR = os.path.join (BUILD_ROOT, '%s-%s' % (module.name, module['Version']))
+    BUILD_DIR = os.path.join(chroot.get_home_dir(), 'tmp-build-dir',
+                             '%s-%s' % (module.name, module['Version']))
     DEBIAN_DIR = os.path.join('autobuild', 'debian', 'debian-%s' % module.name)
 
     code = chroot.system('rm -rf /home/%s/tmp-build-dir' % chroot.user, root=True)
     check_code(code, module)
 
-    code = system('mkdir -p %s' % BUILD_ROOT)
+    code = system('mkdir -p %s' % os.path.dirname(BUILD_DIR))
     check_code(code, module)
 
-    uncompress_tarball(module, BUILD_ROOT)
+    uncompress_tarball(module, BUILD_DIR)
 
     # Kernel packages have to be handled specially.
     if module['Module'] == 'linux':
@@ -57,9 +63,6 @@ def build(chroot, module):
         check_code(code, module)
 
     else:
-        code = system('mv %s/* %s' % (BUILD_ROOT, BUILD_DIR))
-        #check_code(code, module)  FIXME: we don't want to fail when "cannot move to itself"
-
         code = system('cp -r %s %s/debian' % (DEBIAN_DIR, BUILD_DIR))
         check_code(code, module)
 
@@ -94,7 +97,7 @@ def build(chroot, module):
                 code = chroot.system("dpkg -i %s" % chroot.abspath('~/tmp-build-dir/%s' % package), root=True)
                 check_code(code, module)
 
-    code = system('mv -f %s/*.deb repository/' % BUILD_ROOT)
+    code = system('mv -f %s/*.deb repository/' % os.path.dirname(BUILD_DIR))
 
 
 def main(chroot, moduleset, args):
