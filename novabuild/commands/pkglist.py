b6b7e2eb21f556e8f631cb354436d003424d9578
Steve Frécinaux <frecinaux@novacom.be>
2008-09-04 09:03:51 +0200

    Fix 'novabuild list'.
    It raised an exception.
    Also, now the modules are sorted by category, then by name.

--- novabuild/commands/pkglist.py
+++ novabuild/commands/pkglist.py
@@ -1,10 +1,22 @@
 # -*- coding: utf-8 -*- ex:set ts=4 et:
 
+# Function to be used with sort or sorted
+# It compares two dict by looking at the specified fields in order.
+def cmp_by(*fields):
+    def cmpfunc(m1, m2):
+        for f in fields:
+            c = cmp(m1[f], m2[f])
+            if c != 0: return c
+        return 0
+    return cmpfunc
+
 def list_packages(moduleset):
     print "Category           Package Name                 Version         Source URI"
     print "---------          -------------                --------        -----------"
 
-    for module in moduleset:
+    print moduleset.sections.values()
+
+    for module in sorted(moduleset._sections.values(), cmp_by('Category', 'Module')):
         items = {
             'category': module['Category'],
             'name': module.name,
